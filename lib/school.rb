07c10d32f597e906d778a8159408c2928b1e405f
john beltrez <beltrezjohn@gmail.com>
2020-07-27 21:48:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,7 +20,7 @@ class School
   def sort_names
       roster.each do |grade, names|
       sorted_names << names.sort
-      roster = [grade]sorted_names
+      roster = [grade][sorted_names]
     end
     
   end 
